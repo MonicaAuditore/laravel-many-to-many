4cd600b622ea3477728c370647d432b434df5cf2
MonicaAuditore <mauditore@live.it>
2023-03-27 17:38:32 +0200

    add_tabella tecnology

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -5,6 +5,12 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// Model
+use App\Models\Technology;
+
+// Helpers
+use Illuminate\Support\Str;
+
 class TechnologySeeder extends Seeder
 {
     /**
@@ -14,6 +20,19 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $Technologies = [
+            'html',
+            'css',
+            'javascript', 
+            'vue',
+            'laravel'
+        ];
+
+        foreach ($Technologies as $technology) {
+            $newTechnology = Technology::create([
+                'name' => $technology,
+                'slug' => Str::slug($technology),
+            ]);
+        }
     }
 }
